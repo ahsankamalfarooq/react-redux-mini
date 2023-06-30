b03ebea6558aaa7e092b2627eca95af86941de99
Wazir <ahsankamal25@gmail.com>
2023-06-30 15:38:28 +0500

    store created

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import accountReducer from './reducers/account';
+import bonusReducer from './reducers/bonus';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+
+const store = createStore(combineReducers({
+  account : accountReducer,
+  bonus : bonusReducer
+})
+  , applyMiddleware(logger.default, thunk.default))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>
 );
 
